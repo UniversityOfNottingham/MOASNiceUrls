d6e7729c93793cc742b5ba8beb9d0c8bbaa83ca6
James Hodgson <james.hodgson@nottingham.ac.uk>
2016-07-01 09:21:22 +0100

    Fix slug input not being modified for collections

--- libraries/MOASNiceUrls/Controller/Plugin/SlugGenerateFilter.php
+++ libraries/MOASNiceUrls/Controller/Plugin/SlugGenerateFilter.php
@@ -18,9 +18,9 @@ class MOASNiceUrls_Controller_Plugin_SlugGenerateFilter extends Zend_Controller_
      */
     protected $_defaultRoutes = array(
         array('module' => 'default', 'controller' => 'items',
-            'actions' => array('add', 'edit', 'change-type')),
-        array('module' => 'default', 'controller' => 'collection',
-            'actions' => array('add', 'edit', 'change-type')),
+            'actions' => array('add', 'edit'), 'type' => 'Item'),
+        array('module' => 'default', 'controller' => 'collections',
+            'actions' => array('add', 'edit'), 'type' => 'Collection'),
         array('module' => 'default', 'controller' => 'elements',
             'actions' => array('element-form'))
     );
@@ -35,24 +35,26 @@ class MOASNiceUrls_Controller_Plugin_SlugGenerateFilter extends Zend_Controller_
         $db = get_db();
 
         // Some routes don't have a default module, which resolves to NULL.
-        $currentModule = is_null($request->getModuleName()) ? 'default' : $request->getModuleName();
-        $currentController = $request->getControllerName();
-        $currentAction = $request->getActionName();
+        $currentRoute = array(
+            'module' => is_null($request->getModuleName()) ? 'default' : $request->getModuleName(),
+            'controller' => $request->getControllerName(),
+            'action' => $request->getActionName()
+        );
 
-        $routes = apply_filters('moas_nice_urls_routes', $this->_defaultRoutes);
 
-        foreach ($routes as $route) {
+
+        foreach ($this->_defaultRoutes as $route) {
             // Check registered routed against the current route.
-            if ($route['module'] != $currentModule
-                || $route['controller'] != $currentController
-                || !in_array($currentAction, $route['actions']))
+            if ($route['module'] != $currentRoute['module']
+                || $route['controller'] != $currentRoute['controller']
+                || !in_array($currentRoute['action'], $route['actions']))
             {
                 continue;
             }
 
             $element = $db->getTable('MOASNiceUrlsElement')->getSlugElement();
             $elementSet = $db->getTable('ElementSet')->find($element->element_set_id);
-            add_filter(array('ElementInput', 'Item', $elementSet->name, $element->name),
+            add_filter(array('ElementInput', $route['type'], $elementSet->name, $element->name),
                 array($this, 'filterElementInput'));
 
             break;
